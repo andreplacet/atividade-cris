c1ee4f5a979d5abb91f76e5fac9746676a094065
Andre Luiz Zogaib Relvas <arelvas@vitait.com>
2020-10-05 14:45:48 -0300

    atividade 05-10

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace atividade_cris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fatorial = 1;
+            int n;
+            for (n = 1; n <= 10; n++)
+            {
+                fatorial *= n;
+                Console.WriteLine($"{n} fatorial = {fatorial} ");
+            };
+           
+        }
+    }
+}
